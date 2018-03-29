49e13a794c3df7367877095b7ee143987109096a
SimRouter <33904453+simrouter@users.noreply.github.com>
2018-03-29 12:00:18 +0900

    edit

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-once_require('./common.php'); //reosurce include
+require_once('./_common.php'); //reosurce include
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
+	<!-- Custom CSS -->
 </head>
 <body>
-
+	<div class="container">
+	<nav aria-label="breadcrumb">
+  	<ol class="breadcrumb">
+    	<li class="breadcrumb-item"><a href="#">Home</a></li>
+    	<li class="breadcrumb-item"><a href="#">Library</a></li>
+    	<li class="breadcrumb-item active" aria-current="page">Data</li>
+  	</ol>
+	</nav>
+</div>
 </body>
-<script src=".\src\js\bootstrap.js"></script>
-<script src=".\src\js\bootstrap.min.js"></script>
 </html>
