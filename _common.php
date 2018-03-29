18646441c928e1cec90e02e64f88d458d80bea20
SimRouter <33904453+simrouter@users.noreply.github.com>
2018-03-29 11:44:23 +0900

    source

---
+++ _common.php
@@ -0,0 +1,9 @@
+<head>
+	<title>LVS System</title>
+	<meta charset="utf-8">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href=".\src\css\bootstrap.css">
+	<link rel="stylesheet" type="text/css" href=".\src\css\bootstrap.min.css">
+	<!-- Custom CSS -->
+</head>
